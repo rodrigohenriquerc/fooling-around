9b01f873890065e314cda985a6de82efdff29c5b
Rodrigo Carvalho <rhrc09@gmail.com>
2025-11-09 03:21:59 -0300

    feat: store tracking state using TrackingStorage

--- src/providers/Tracking/TrackingProvider.tsx
+++ src/providers/Tracking/TrackingProvider.tsx
@@ -1,17 +1,38 @@
-import { createContext, PropsWithChildren, useState } from "react";
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
 
-import { Tracking } from "@/types/Tracking.types";
+import { Tracking, TrackingState } from "@/types/Tracking.types";
+import TrackingStorage from "@/storage/TrackingStorage";
 
 export default function TrackingProvider({ children }: PropsWithChildren) {
-  const [state, setState] = useState<Tracking["state"]>("off");
+  const [state, setState] = useState<TrackingState>();
+  const [isInitialized, setIsInitialized] = useState(false);
 
-  const toggle = () => {
+  function toggle() {
     setState((prev) => (prev === "off" ? "on" : "off"));
-  };
+  }
+
+  useEffect(() => {
+    async function init() {
+      const storedState = await TrackingStorage.getState();
+      setState(storedState);
+      setIsInitialized(true);
+    }
+
+    init();
+  }, []);
+
+  useEffect(() => {
+    async function storeToggleState() {
+      if (!state) return;
+      await TrackingStorage.toggle(state);
+    }
+
+    storeToggleState();
+  }, [state]);
 
   return (
-    <TrackingContext.Provider value={{ state, toggle }}>
-      {children}
+    <TrackingContext.Provider value={{ state: state ?? "off", toggle }}>
+      {isInitialized ? children : null}
     </TrackingContext.Provider>
   );
 }

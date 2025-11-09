11ebeae9bf4c48f5fd5e18faef474d038b1d60d7
Rodrigo Carvalho <rhrc09@gmail.com>
2025-11-08 22:26:30 -0300

    feat: add Home

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import HomeStyles from "./Home.styles";
+
+export default function Home() {
+  return (
+    <View style={HomeStyles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+}

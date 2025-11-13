f17badf51230f01ed54b77c0cfaf35f4d089fa5a
Rodrigo Carvalho <rhrc09@gmail.com>
2025-11-13 16:24:39 -0300

    refactor: reset Home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { View } from "react-native";
 
-import DeviceLocationWatcher from "@/device/location";
-
 import { TrackingButton } from "@/components/Tracking";
 
 import styles from "./Home.styles";
@@ -11,17 +9,7 @@ export default function Home() {
   const [state, setState] = useState<"on" | "off">("off");
 
   const onPressTrackingButtonHandler = async () => {
-    try {
-      if (state === "off") {
-        await DeviceLocationWatcher.startWatching();
-        setState("on");
-      } else {
-        await DeviceLocationWatcher.stopWatching();
-        setState("off");
-      }
-    } catch (error) {
-      console.error(error);
-    }
+    setState((prev) => (prev === "off" ? "on" : "off"));
   };
 
   return (

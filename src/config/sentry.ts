83483db96d74957c89a80ece2835e348115e7a83
Rodrigo Carvalho <rhrc09@gmail.com>
2025-12-06 09:14:49 -0300

    chore: update Sentry config

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -2,14 +2,15 @@ import * as Sentry from "@sentry/react-native";
 
 Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
-  sendDefaultPii: true,
+  enabled: !__DEV__,
   enableLogs: true,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
   integrations: [
     Sentry.mobileReplayIntegration(),
     Sentry.feedbackIntegration(),
   ],
+  sendDefaultPii: true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
 });
 
 export default Sentry;

d57ee77dedad2c9652857486cb17546a1ee9a2c5
Rodrigo Carvalho <rhrc09@gmail.com>
2025-12-06 09:26:13 -0300

    refactor: update Logger

--- src/infra/logs/logger.ts
+++ src/infra/logs/logger.ts
@@ -2,11 +2,9 @@ import * as Sentry from "@sentry/react-native";
 
 export const Logger = (() => {
   return {
-    error: (message: unknown, error?: unknown) => {
-      const errorMessage = error ? `${message}: ${error}` : message;
-
-      console.error(errorMessage);
-      Sentry.captureException(new Error(`${errorMessage}`));
+    error: (message: string, error?: unknown) => {
+      console.error(message, error);
+      Sentry.captureException(new Error(message, { cause: error }));
     },
   };
 })();

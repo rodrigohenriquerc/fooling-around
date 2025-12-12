7ebc1e69b2c970222de4f6d759c1cf5f69350e29
Rodrigo Carvalho <rhrc09@gmail.com>
2025-12-12 19:49:29 -0300

    refactor: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import "@/tasks";
 
-import { DatabaseProvider, UIProvider } from "./presentation/providers";
+import { DatabaseProvider } from "@nozbe/watermelondb/react";
+import * as Sentry from "@sentry/react-native";
+import { StatusBar } from "expo-status-bar";
+
+import database from "@/infra/database";
+
 import { HomeScreen } from "./presentation/screens";
-import { Sentry } from "./tools/monitoring";
 
 function App() {
   return (
-    <DatabaseProvider>
-      <UIProvider>
-        <HomeScreen />
-      </UIProvider>
+    <DatabaseProvider database={database}>
+      <StatusBar style="auto" />
+      <HomeScreen />
     </DatabaseProvider>
   );
 }
 
 export default Sentry.wrap(App);
+
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  enableLogs: true,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+  sendDefaultPii: true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+});
